feed65ebc145ab3e042146adffa15b22769c3a9b
Parashuram N <axemclion@users.noreply.github.com>
2022-02-11 08:49:06 -0800

    Update mac-setup.sh
    Changed key repeat speeds

--- home/mac-setup.sh
+++ home/mac-setup.sh
@@ -152,8 +152,8 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 10
-defaults write NSGlobalDomain InitialKeyRepeat -int 100
+defaults write NSGlobalDomain KeyRepeat -int 3
+defaults write NSGlobalDomain InitialKeyRepeat -int 50
 
 # Set language and text formats
 # Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
